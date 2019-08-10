a1fb0872efd91388f0c59f1545e22b85e50f2786
jenny obryant <jennyobryant@protonmail.com>
2019-08-09 22:41:27 -0600

    final draft

--- js/index.js
+++ js/index.js
@@ -28,7 +28,7 @@ const siteContent = {
   },
   "contact": {
     "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street Somewhere, USA",
+    "address" : "123 Way 456 Street<br>Somewhere, USA",
     "phone" : "1 (888) 888-8888",
     "email" : "sales@greatidea.io",
   },
@@ -63,6 +63,61 @@ let ctaImage = document.querySelector("#cta-img");
 ctaImage.setAttribute('src', siteContent.cta['img-src']);  
 
 //body content
-let mainContentFeatures = document.querySelector(".top-content .text-content h4"); 
-mainContentFeatures.textContent = siteContent['main-content']['features-h4']; 
+let Features = document.querySelector(".top-content .text-content h4"); 
+Features.textContent = siteContent['main-content']['features-h4']; 
+let Featuresparagraph = document.querySelector(".top-content .text-content p"); 
+Featuresparagraph.textContent = siteContent['main-content']['features-content']; 
 
+let about = document.querySelectorAll(".top-content .text-content h4")[1];
+about.textContent = siteContent['main-content']['about-h4']; 
+let aboutparagraph = document.querySelectorAll(".top-content .text-content p")[1]; 
+aboutparagraph.textContent = siteContent['main-content']['about-content']; 
+
+//image appear
+let Image = document.querySelector("#middle-img"); 
+Image.setAttribute('src', siteContent['main-content']['middle-img-src']);
+
+//bottom element
+let bottomHeaders = document.querySelectorAll('.bottom-content h4');
+let servicesHeader = bottomHeaders[0];
+let productHeader = bottomHeaders[1];
+let visionHeader = bottomHeaders[2]; 
+
+servicesHeader.textContent = siteContent['main-content']['services-h4']; 
+productHeader.textContent = siteContent['main-content']['product-h4']; 
+visionHeader.textContent = siteContent['main-content']['vision-h4']; 
+
+let bottomPara = document.querySelectorAll('.bottom-content p');
+let servicesPara = bottomPara[0];
+let productPara = bottomPara[1];
+let visionPara = bottomPara[2]; 
+
+servicesPara.textContent = siteContent['main-content']['services-content']; 
+productPara.textContent = siteContent['main-content']['product-content']; 
+visionPara.textContent = siteContent['main-content']['vision-content']; 
+
+//contact
+let contact = document.querySelector(".contact h4"); 
+contact.textContent = siteContent['contact']['contact-h4']; 
+
+let contactPara = document.querySelectorAll('.contact p');
+contactPara[0].innerHTML= siteContent.contact.address; 
+contactPara[1].textContent= siteContent.contact.phone; contactPara[2].textContent= siteContent.contact.email; 
+
+//footer
+let footer = document.querySelector("footer p"); 
+footer.textContent = siteContent['footer']['copyright'];
+
+//nav becomes green
+document.querySelectorAll("a").forEach(a =>
+  a.style.color = "green");
+
+//appendchild and prepend (blog or more)
+let nav = document.querySelector('nav');
+let newone = document.createElement('a');
+newone.textContent = "blog"; 
+nav.prepend(newone); 
+
+newone = document.createElement('a');
+newone.textContent = "more"; 
+nav.appendChild(newone); 
